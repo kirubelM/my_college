75e5d4434bc4121d651bded1d1edf509d3107eec
kirubelM <kirubelmognehode18@augustana.edu>
2021-04-08 00:55:25 -0500

    Add deleting feature and deployed it to database

--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,18 @@
 import { List, ListItemText, ListItem, ListItemAvatar} from '@material-ui/core'
 import './Todo.css';
 import React from 'react'
-
+import db from './firebase';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 function Todo(props) {
     return (
         <List className="todo__list">
             <ListItem>
                 <ListItemAvatar>
                 </ListItemAvatar>
-                <ListItemText primary={props.text} secondary="Dummy Deadline⏰" />
+                <ListItemText primary={props.todo.todo} secondary="Dummy Deadline⏰" />
             </ListItem>
+            <DeleteIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}>Delete⛔</DeleteIcon>
         </List>
     )
 }
