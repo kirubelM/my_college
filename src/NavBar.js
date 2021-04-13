18cf7b4707836f7adaa3eabbde4dcf29dbf9ffb8
kirubelM <kirubelmognehode18@augustana.edu>
2021-04-13 03:01:54 -0500

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -34,7 +34,6 @@ export default function ButtonAppBar() {
               Collegify
           </Typography>
           <Button color="inherit"><ExitToAppIcon>Logout</ExitToAppIcon></Button>
-         
         </Toolbar>
       </AppBar>
     </div>
